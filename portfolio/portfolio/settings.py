fc03484eb4bd76a49f6261baea7b9f23824d0cac
unknown <pieternella.luttikhuizen@nioz.nl>
2018-09-28 16:13:41 +0200

    minor update

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = os.environ["PORTFOLIO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.pythonanywhere.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
